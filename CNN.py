a2cb0ca294d658fd78c50099b60d33bea91c1fec
apurba dipto <apurbadipto@cb215.loc>
2024-11-14 13:55:46 -0330

    1st commit

--- CNN.py
+++ CNN.py
@@ -19,8 +19,6 @@ mnist = tf.keras.datasets.mnist
 x_train, x_test = x_train/255.0, x_test/255.0
 x_train, x_test = np.expand_dims(x_train, axis=-1), np.expand_dims(x_test, axis=-1)
 
-print("Hello World")
-
 ## Model
 inputs = KL.Input(shape=(28, 28, 1))
 c = KL.Conv2D(32, (3, 3), padding="valid", activation=tf.nn.relu)(inputs)
